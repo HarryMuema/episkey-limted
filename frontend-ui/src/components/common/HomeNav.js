f8391bf9e27ee774c93cca8bd40bad98bda688ec
Harry Muema <harymueema@gmail.com>
2022-07-20 05:03:18 +0300

    added the react-router-dom and tailwindcss

---
+++ frontend-ui/src/components/common/HomeNav.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HomeNav = () => {
+  return (
+    <div class="text-[500px] text-3xl bg-red">hello worlddddsds</div>
+  )
+}
+
+export default HomeNav
--- frontend-ui/src/index.js
+++ frontend-ui/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {BrowserRouter,Route,Routes} from 'react-router-dom'
+import HomeNav from './components/common/HomeNav';
 import './index.css';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    hello world
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<HomeNav/>} />
+      </Routes>
+     </BrowserRouter> 
   </React.StrictMode>
 );
